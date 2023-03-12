98484f74af18cf6152f50f1aa1b79248d4b25bfa
Yuvaraj C U <108235041+YuvarajCU@users.noreply.github.com>
2023-03-12 12:09:20 +0530

    Update Laptop_Price_Predictor-2.py

--- Laptop_Price_Predictor-2.py
+++ Laptop_Price_Predictor-2.py
@@ -25,6 +25,8 @@ weight = st.number_input("Weight of the Laptop")
 touchscreen = st.selectbox("TouchScreen", ['No', 'Yes'])
 #IPS
 ips = st.selectbox("IPS", ['No', 'Yes'])
+#screen size
+screen_size = st.number_input('Screen Size in Inches')
 # resolution
 resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
 #cpu
